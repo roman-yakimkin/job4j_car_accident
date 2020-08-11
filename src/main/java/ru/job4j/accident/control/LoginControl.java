cb1a0d6884a697cea6be2a93fac3bd92674928d1
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-08-11 21:56:45 +0300

    Spring Security[6879#297358]

---
+++ src/main/java/ru/job4j/accident/control/LoginControl.java
@@ -0,0 +1,47 @@
+package ru.job4j.accident.control;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The login controller class
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 11.08.2020
+ * @version 1.0
+ */
+@Controller
+public class LoginControl {
+    @GetMapping("/login")
+    public String loginPage(@RequestParam(value = "error", required = false) String error,
+                            @RequestParam(value = "logout", required = false) String logout,
+                            Model model) {
+        String errMsg = null;
+        if (error != null) {
+            errMsg = "Username or Password is incorrect !!";
+        }
+        if (logout != null) {
+            errMsg = "You have been successfully logged out !!";
+        }
+        model.addAttribute("errMsg", errMsg);
+        return "login";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
+    }
+}
